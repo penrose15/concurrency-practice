331d8af14582159aedccfc64b04a1bc51dca1253
penrose15 <seongju99@naver.com>
2023-04-29 00:04:24 +0900

    first commit

---
+++ src/main/java/com/example/concurencypractice/ConcurencyPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurencypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurencyPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurencyPracticeApplication.class, args);
+	}
+
+}
